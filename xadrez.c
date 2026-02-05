d69bf49a5900ea1fc9108fa79dc949ecf8a64386
GeorgeOficial <georgeantonio420@gmail.com>
2026-02-05 19:05:28 +0000

    Movimentação do Cavalo

--- xadrez.c
+++ xadrez.c
@@ -3,8 +3,6 @@ int main(){
 
     char dir[4][10] = {"Direita", "Esquerda", "Cima", "Baixo"};
     int steps;
-    int feets = 0;
-    int pes = 0;
 
     /*
     Torre terá que se mover 5 para direita (for)
@@ -12,20 +10,34 @@ int main(){
     Rainha terá que se mover 8 para direita (do-while)
     */
     
-    for(steps=0; steps < 5; steps++){
+    for(steps = 0; steps < 5; steps++){
         printf("%s\n", dir[0]);
-    } printf("Torre se moveu %d vezes\n\n", steps);
+    } printf("Torre se moveu %d casas\n\n", steps);
 
-    while(feets < 5){
+    steps = 0;
+
+    while(steps < 5){
         printf("%s + %s\n", dir[2], dir[0]);
-        feets++;
-    } printf("Bispo se moveu %d vezes\n\n", feets);
+        steps++;
+    } printf("Bispo se moveu %d casas\n\n", steps);
+
+    steps = 0;
 
     do {
         printf("%s\n", dir[1]);
-        pes++;
-    } while (pes < 8);
-    printf("Rainha se moveu %d vezes\n\n", pes);
+        steps++;
+    } while (steps < 8);
+    printf("Rainha se moveu %d casas\n\n", steps);
+
+    steps = 0;
+
+    for (steps; steps < 1; steps++){ 
+        while (steps < 2){
+            printf("%s\n", dir[3]);
+            steps++;
+        }
+        printf("%s\n", dir[1]);
+    } printf("Cavalo se moveu %d casas\n\n", steps);
 
 return 0;
 }
