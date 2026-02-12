b733bd66482425a097787a9826ed6fd92692a884
GeorgeOficial <georgeantonio420@gmail.com>
2026-02-12 03:17:24 +0000

    Xadrez Nível Mestre: Torre Pronta

--- xadrez.c
+++ xadrez.c
@@ -1,43 +1,71 @@
 #include <stdio.h>
+#include <string.h>
+
+void torre(char d[], int s, char p[]){
+    if (strcmp(d, "Cima + Direita") == 0 ||
+        strcmp(d, "Cima + Esquerda") == 0 ||
+        strcmp(d, "Baixo + Direita") == 0 ||
+        strcmp(d, "Baixo + Esquerda") == 0)
+        {
+            printf("Torre não se move em diagonais\n");
+            return;
+        } 
+        if (s>0){
+            printf("%s \n", d);
+            torre(d, s-1, p);
+            printf("d");
+        }
+}
+
 int main(){
 
-    char dir[4][10] = {"Direita", "Esquerda", "Cima", "Baixo"};
+    char piece[4][20] = {"a Torre", "o Bispo", "a Rainha", "o Cavalo"};
+    char dir[20];
     int steps;
 
     /*
-    Torre terá que se mover 5 para direita (for)
-    Bispo terá que se mover 5 para cima + direita (while)
-    Rainha terá que se mover 8 para direita (do-while)
+    Utilizar scanf() para fazer com que as peças se movam de acordo com as coordenadas desejadas;
+    ex para verticais e horizontais: W = cima; S = baixo; D = direita; A = esquerda;
+    ex para diagonais: Cima = WD, WA; Baixo = SD, SA;
+    E declarar o número de casas em outro scanf();
+    Utilizar recursividade e funções;
     */
+
+    printf("Demarque uma direção com WASD para %s:\n", piece[0]);
+    scanf("%s", dir);
+    printf("\n");
+    printf("Selecione o número de casas para a Torre:\n");
+    scanf("%d", &steps);
+    printf("\n");
+
+    if (strcmp(dir, "d") == 0){ //strcmp simplesmente compara a string. Caso fosse ((dir, "d") > 0) verificaria se a string vale "e"
+     strcpy(dir, "Direita");
+    }
+    if (strcmp(dir, "a") == 0){
+     strcpy(dir, "Esquerda");
+    }
+    if (strcmp(dir, "w") == 0){
+     strcpy(dir, "Cima");
+    }
+    if (strcmp(dir, "s") == 0){
+     strcpy(dir, "Baixo");
+    }
+    if (strcmp(dir, "wa") == 0 || strcmp(dir, "aw") == 0){
+     strcpy(dir, "Cima + Esquerda");
+    }
+    if (strcmp(dir, "wd") == 0 || strcmp(dir, "dw") == 0){
+     strcpy(dir, "Cima + Direita");
+    }
+    if (strcmp(dir, "sa") == 0 || strcmp(dir, "as") == 0){
+     strcpy(dir, "Baixo + Esquerda");
+    }
+    if (strcmp(dir, "sd") == 0 || strcmp(dir, "ds") == 0){
+     strcpy(dir, "Baixo + Direita");
+    }
+
+    torre(dir, steps, piece);
+
     
-    for(steps = 0; steps < 5; steps++){
-        printf("%s\n", dir[0]);
-    } printf("Torre se moveu %d casas\n\n", steps);
-
-    steps = 0;
-
-    while(steps < 5){
-        printf("%s + %s\n", dir[2], dir[0]);
-        steps++;
-    } printf("Bispo se moveu %d casas\n\n", steps);
-
-    steps = 0;
-
-    do {
-        printf("%s\n", dir[1]);
-        steps++;
-    } while (steps < 8);
-    printf("Rainha se moveu %d casas\n\n", steps);
-
-    steps = 0;
-
-    for (steps; steps < 1; steps++){ 
-        while (steps < 2){
-            printf("%s\n", dir[3]);
-            steps++;
-        }
-        printf("%s\n", dir[1]);
-    } printf("Cavalo se moveu %d casas\n\n", steps);
 
 return 0;
 }
