23b13983a73eb9fd54bcd767fadf908610a5d175
GeorgeOficial <georgeantonio420@gmail.com>
2026-02-13 04:54:11 +0000

    Movimentação do cavalo

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Aplicar estruturas de repetição simples (for, while, do-while) para simular mo
  
 Aplicar loops aninhados para simular o movimento complexo em "L" do Cavalo no tabuleiro de xadrez.
  
-Aplicar recursividade e loops complexos com múltiplas condições para simular movimentos avançados das peças de xadrez. 
+Aplicar recursividade e loops complexos com múltiplas condições para simular movimentos avançados das peças de xadrez.
--- xadrez.c
+++ xadrez.c
@@ -1,27 +1,104 @@
 #include <stdio.h>
 #include <string.h>
 
-void torre(char d[], int s, char p[]){
+int steps;
+
+void posicao(char d[]){
+    if (strcmp(d, "d") == 0){ //strcmp simplesmente compara a string. Caso fosse ((dir, "d") > 0) verificaria se a string vale "e"
+     strcpy(d, "Direita");
+    }
+    if (strcmp(d, "a") == 0){
+     strcpy(d, "Esquerda");
+    }
+    if (strcmp(d, "w") == 0){
+     strcpy(d, "Cima");
+    }
+    if (strcmp(d, "s") == 0){
+     strcpy(d, "Baixo");
+    }
+    if (strcmp(d, "wa") == 0 || strcmp(d, "aw") == 0){
+     strcpy(d, "Cima + Esquerda");
+    }
+    if (strcmp(d, "wd") == 0 || strcmp(d, "dw") == 0){
+     strcpy(d, "Cima + Direita");
+    }
+    if (strcmp(d, "sa") == 0 || strcmp(d, "as") == 0){
+     strcpy(d, "Baixo + Esquerda");
+    }
+    if (strcmp(d, "sd") == 0 || strcmp(d, "ds") == 0){
+     strcpy(d, "Baixo + Direita");
+    }
+}
+
+void torre(char d[], int s){
     if (strcmp(d, "Cima + Direita") == 0 ||
         strcmp(d, "Cima + Esquerda") == 0 ||
         strcmp(d, "Baixo + Direita") == 0 ||
         strcmp(d, "Baixo + Esquerda") == 0)
         {
-            printf("Torre não se move em diagonais\n");
+            printf("Torre não se move em diagonais\n\n");
             return;
-        } 
+        }
         if (s>0){
             printf("%s \n", d);
-            torre(d, s-1, p);
-            printf("d");
+            torre(d, s-1);
         }
+        if (s==steps){
+            printf("Torre se moveu %d casas para %s \n\n", s, d);
+        }
+}
+
+void rainha(char d[], int s){
+    if (s>0){
+            printf("%s \n", d);
+            rainha(d, s-1);
+        }
+        if (s==steps){
+            printf("Rainha se moveu %d casas para %s \n\n", s, d);
+        }
+}
+
+void bispo(char d[], int s){
+    if (strcmp(d, "Direita") == 0 ||
+        strcmp(d, "Cima") == 0 ||
+        strcmp(d, "Baixo") == 0 ||
+        strcmp(d, "Esquerda") == 0)
+        {
+            printf("Bispo não se move em verticais ou horizontais\n\n");
+            return;
+        }
+        if (s>0){
+            printf("%s \n", d);
+            torre(d, s-1);
+        }
+        if (s==steps){
+            printf("Bispo se moveu %d casas para %s \n\n", s, d);
+        }
+}
+
+void cavalo(char d[]){
+    if (strcmp(d, "Direita") == 0 ||
+        strcmp(d, "Cima") == 0 ||
+        strcmp(d, "Baixo") == 0 ||
+        strcmp(d, "Esquerda") == 0)
+        {
+            printf("Cavalo não se move em verticais ou horizontais\n\n");
+            return;
+        } else{
+            if(strcmp()){
+            for(int i = 0; i < 1; i++){
+                for(int j=0; j<2; j++){
+                    printf("cima");
+                }
+                printf("direita");
+            }
+        }
+    }
 }
 
 int main(){
 
-    char piece[4][20] = {"a Torre", "o Bispo", "a Rainha", "o Cavalo"};
     char dir[20];
-    int steps;
 
     /*
     Utilizar scanf() para fazer com que as peças se movam de acordo com as coordenadas desejadas;
@@ -31,41 +108,49 @@ int main(){
     Utilizar recursividade e funções;
     */
 
-    printf("Demarque uma direção com WASD para %s:\n", piece[0]);
+    printf("Demarque uma direção com WASD para a Torre:\n");
     scanf("%s", dir);
     printf("\n");
     printf("Selecione o número de casas para a Torre:\n");
     scanf("%d", &steps);
     printf("\n");
 
-    if (strcmp(dir, "d") == 0){ //strcmp simplesmente compara a string. Caso fosse ((dir, "d") > 0) verificaria se a string vale "e"
-     strcpy(dir, "Direita");
-    }
-    if (strcmp(dir, "a") == 0){
-     strcpy(dir, "Esquerda");
-    }
-    if (strcmp(dir, "w") == 0){
-     strcpy(dir, "Cima");
-    }
-    if (strcmp(dir, "s") == 0){
-     strcpy(dir, "Baixo");
-    }
-    if (strcmp(dir, "wa") == 0 || strcmp(dir, "aw") == 0){
-     strcpy(dir, "Cima + Esquerda");
-    }
-    if (strcmp(dir, "wd") == 0 || strcmp(dir, "dw") == 0){
-     strcpy(dir, "Cima + Direita");
-    }
-    if (strcmp(dir, "sa") == 0 || strcmp(dir, "as") == 0){
-     strcpy(dir, "Baixo + Esquerda");
-    }
-    if (strcmp(dir, "sd") == 0 || strcmp(dir, "ds") == 0){
-     strcpy(dir, "Baixo + Direita");
-    }
+    posicao(dir);
 
-    torre(dir, steps, piece);
-
+    torre(dir, steps);
     
+
+    printf("Demarque uma direção com WASD para a Rainha:\n");
+    scanf("%s", dir);
+    printf("\n");
+    printf("Selecione o número de casas para a Rainha:\n");
+    scanf("%d", &steps);
+    printf("\n");
+
+    posicao(dir);
+
+    rainha(dir, steps);
+
+
+    printf("Demarque uma direção com WASD para o Bispo:\n");
+    scanf("%s", dir);
+    printf("\n");
+    printf("Selecione o número de casas para o Bispo:\n");
+    scanf("%d", &steps);
+    printf("\n");
+
+    posicao(dir);
+
+    bispo(dir, steps);
+
+
+    printf("Demarque uma direção com WASD para o Cavalo:\n");
+    scanf("%s", dir);
+    printf("\n");
+
+    posicao(dir);
+
+    cavalo(dir);
 
 return 0;
 }
